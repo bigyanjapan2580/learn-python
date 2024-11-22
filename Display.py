6347faf80df8396a4440d0ea975d074ab8b048eb
BIGYAN POUDEL <poudelbigyan096@gmail.com>
2024-11-22 20:20:35 +0900

    self introduction

--- Display.py
+++ Display.py
@@ -1,2 +1,7 @@
 
-print("i love nepal")
+print("i am bigyan poudel")
+print("i live in japan")
+print("i am 24 old year")
+print("my hometown is nepal")
+print("now i am reading electrical system")
+print("i am intrested in programming hacking")
