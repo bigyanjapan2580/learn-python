4797fefa049f061f0521dcf3957acc5e6e6598db
BIGYAN POUDEL <poudelbigyan096@gmail.com>
2024-12-02 23:35:40 +0900

    print name in assanding&desshinge order

--- 01_datatypes/numbers.py
+++ 01_datatypes/numbers.py
@@ -1,3 +1,14 @@
-a=int(input("enter the first value"))
-b=int(input("enter the second value"))
-print("the sume the value of a & b =",a+b)
+a="bigyan"
+print(a)
+print(a[0:6])
+print(a[0:5])
+print(a[0:4])
+print(a[0:3])
+print(a[0:2])
+print(a[0:1])
+
+print(a[0:-5])
+print(a[0:-4])
+print(a[0:-3])
+print(a[0:-2])
+print(a[0:-1])
