304b90d1be061fec237efe96098fd23a21da1f79
BIGYAN POUDEL <poudelbigyan096@gmail.com>
2024-12-21 20:30:05 +0900

    equation=

--- 01_datatypes/numbers.py
+++ 01_datatypes/numbers.py
@@ -1,14 +1,4 @@
-a="bigyan"
-print(a)
-print(a[0:6])
-print(a[0:5])
-print(a[0:4])
-print(a[0:3])
-print(a[0:2])
-print(a[0:1])
-
-print(a[0:-5])
-print(a[0:-4])
-print(a[0:-3])
-print(a[0:-2])
-print(a[0:-1])
+a=10
+b=20
+c=30
+print("the equation=",(a+b)/c)
