45372cb5fd13550099f226d715d1b162ea37c8b7
BIGYAN POUDEL <poudelbigyan096@gmail.com>
2024-12-28 11:36:01 +0900

    multiline comment ,single word ,find the class or type

---
+++ 01_datatypes/Strings.py
@@ -0,0 +1,15 @@
+#3quotes for multipurpose
+x="""i am bigyan poudel
+i am student 
+"""
+print (x)
+
+#print single word
+print(x[1])
+print(x[2])
+print(x[4],x[5])
+
+#none  (type to find the class)
+y="None" #string
+y=None #none
+print(type(y))
